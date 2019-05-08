c3cdf81b52bad568a0d5b25c0f96b642975c116b
Luca Candela <ilcande_prime@live.it>
2019-05-08 22:18:29 +0200

    install babel and dependencies

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
-'use strict';
-
 console.log('App.js is running');
 
 // JSX - Javascript XML
 
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
-);
+var template = <h1>Indecision App</h1>;
 
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
