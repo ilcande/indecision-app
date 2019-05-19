bd16bcbe97d9c330e04caddc95f178b82b5ca30e
Luca Candela <ilcande_prime@live.it>
2019-05-19 11:05:07 +0200

    play with jsx esxpressions and conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,78 @@
 console.log('App.js is running');
 
 // JSX - Javascript XML
+var app = {
+  title: 'Indecision App',
+  subtitle: 'A simple TODO list built in React',
+  options: ['One', 'Two']
+};
 
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'these are your available options' : 'No options available'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+var user = {
+  name: 'Luca',
+  age: 31,
+  location: 'Foggia'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,8 +1,43 @@
 console.log('App.js is running');
 
 // JSX - Javascript XML
+var app = {
+  title: 'Indecision App',
+  subtitle: 'A simple TODO list built in React',
+  options: ['One', 'Two']
+}
 
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'these are your available options' : 'No options available'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: 'Luca',
+  age: 31,
+  location: 'Foggia'
+};
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+var templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
 
 var appRoot = document.getElementById('app');
 
