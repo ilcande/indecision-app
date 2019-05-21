fc20217258037b3137485a68822c04042e9125dd
Luca Candela <ilcande_prime@live.it>
2019-05-21 20:26:06 +0200

    learning about events and attributes ans manual data binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,37 +1,112 @@
 'use strict';
 
-var add = function add(a, b) {
-  // console.log(arguments);
-  return a + b;
+console.log('App.js is running');
+
+// JSX - Javascript XML
+var app = {
+  title: 'Indecision App',
+  subtitle: 'A simple TODO list built in React',
+  options: ['One', 'Two']
 };
 
-console.log(add(20, 30));
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'these are your available options' : 'No options available'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
 
-var user = {
-  name: 'Luca',
-  cities: ['Sydney', 'Barcelona', 'London'],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    });
-    // this.cities.forEach((city) => {
-    //   console.log(this.name + ' has lived in ' + city)
-    // });
-  }
+var count = 0;
+var addOne = function addOne() {
+  count++;
+  renderCounterApp();
+};
+var minusOne = function minusOne() {
+  count -= 1;
+  renderCounterApp();
+};
+var resetCounter = function resetCounter() {
+  count = 0;
+  renderCounterApp();
 };
 
-console.log(user.printPlacesLived());
+var appRoot = document.getElementById('app');
 
-var multiplier = {
-  numbers: [2, 4, 6],
-  multiplyBy: 3,
-  multiply: function multiply() {
-    return this.numbers.map(function (number) {
-      return number * number;
-    });
-  }
+var renderCounterApp = function renderCounterApp() {
+  var templateThree = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: resetCounter },
+      'Reset'
+    )
+  );
+
+  ReactDOM.render(templateThree, appRoot);
 };
 
-console.log(multiplier.multiply());
+renderCounterApp();
+
+// const user = {
+//   name: 'Luca',
+//   age: 31,
+//   location: 'Foggia'
+// };
+
+// function getLocation(location) {
+//   if (location) {
+//     return <p>Location: {location}</p>;
+//   }
+// }
+
+// const templateTwo = (
+//   <div>
+//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
+//     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+//     {getLocation(user.location)}
+//   </div>
+// );
