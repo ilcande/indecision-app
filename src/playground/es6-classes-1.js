45237b0641a9907f631ee3b3442253afa220f85e
Luca Candela <ilcande_prime@live.it>
2019-05-26 15:27:43 +0200

    learn about subclasses, constructor method and super keyword

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -7,12 +7,47 @@ class Person {
     return `Hi! I am ${this.name}`;
   }
   getDescription() {
-    return `${this.name} is ${this.age} year(s) old`;
+    return `${this.name} is ${this.age} year(s) old.`;
   }
 }
 
-const me = new Person('Luca', 31);
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
 
-const other = new Person();
-console.log(other.getDescription());
+    if (this.hasMajor) {
+      description = description += ` His/Her major is ${this.major}`;
+    }
+
+    return description;
+  }
+}
+
+class Traveller extends Person {
+  constructor(name, age, major, homeLocation) {
+    super(name, age, major);
+    this.homeLocation = homeLocation;
+  }
+  getGreeting() {
+    let location = super.getGreeting();
+
+    if (this.homeLocation) {
+      location = location += ` I'm visiting from ${this.homeLocation}`;
+    }
+
+    return location;
+  }
+}
+
+const me = new Traveller('Luca', 31, 'Software Engineering', 'Barcelona');
+console.log(me.getGreeting());
+
+const other = new Traveller();
+console.log(other.getGreeting());
