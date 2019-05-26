073fe4cb326c73b4abd9447a2c81ed417bdcfbf5
Luca Candela <ilcande_prime@live.it>
2019-05-26 12:02:13 +0200

    start to understand how es6 classes work in react

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,41 +1,37 @@
 'use strict';
 
-console.log('Toggle Visibility.js is running');
-var app = {
-  title: 'Visibility Toggle',
-  visibility: false
-};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var onToggle = function onToggle() {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  app.visibility = !app.visibility;
-  console.log('clicked');
-  renderToggleVisibility();
-};
+var Person = function () {
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var renderToggleVisibility = function renderToggleVisibility() {
-  var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      app.title
-    ),
-    React.createElement(
-      'button',
-      { onClick: onToggle },
-      app.visibility === false ? 'Show' : 'Hide'
-    ),
-    React.createElement(
-      'p',
-      null,
-      app.visibility === false ? '' : 'Details here'
-    )
-  );
+    _classCallCheck(this, Person);
 
-  ReactDOM.render(template, appRoot);
-};
+    this.name = name;
+    this.age = age;
+  }
 
-var appRoot = document.getElementById('app');
-renderToggleVisibility();
+  _createClass(Person, [{
+    key: 'getGreeting',
+    value: function getGreeting() {
+      return 'Hi! I am ' + this.name;
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.name + ' is ' + this.age + ' year(s) old';
+    }
+  }]);
+
+  return Person;
+}();
+
+var me = new Person('Luca', 31);
+console.log(me.getDescription());
+
+var other = new Person();
+console.log(other.getDescription());
---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,18 @@
+class Person {
+  constructor(name = 'Anonymous', age = 0) {
+    this.name = name;
+    this.age = age;
+  }
+  getGreeting() {
+    return `Hi! I am ${this.name}`;
+  }
+  getDescription() {
+    return `${this.name} is ${this.age} year(s) old`;
+  }
+}
+
+const me = new Person('Luca', 31);
+console.log(me.getDescription());
+
+const other = new Person();
+console.log(other.getDescription());
